35163a99bb32562e88e83d5f51c7220b1742f56a
Easton Park <128500186+cali-rocket@users.noreply.github.com>
2023-06-05 10:52:55 +0900

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,28 +1,9 @@
 const express = require("express");
 const passport = require("passport");
-const jwt = require("jsonwebtoken");
+const { generateToken } = require("../middlewares/auth");
 const router = express.Router();
 
 router.get("/kakao", passport.authenticate("kakao"));
-
-// router.get(
-//     "/kakao/callback",
-//     passport.authenticate("kakao", {
-//         failureRedirect: "/",
-//     }),
-//     async(req, res) => {
-//         var { kakao_id } = req.user;
-//         console.log(`테스트`+ kakao_id)
-//         const user = await Users.findOne({ where: { kakao_id } });
-//         var token = jwt.sign({ user_id: Users.user_id }, 'firstclass')
-//         Users.user_id = req.query.user_id
-//         res.send('token:'+Users.user_id)
-//         //res.redirect("http://localhost:3001")
-//         // return res
-//         //     .status(200)
-//         //     .json({ token: `Bearer ${token}`, message: "로그인 성공" })
-//     }
-// );
 
 const kakaoCallback = (req, res, next) => {
     passport.authenticate(
@@ -30,14 +11,17 @@ const kakaoCallback = (req, res, next) => {
         { failureRedirect: "/" },
         (err, user, info) => {
             if (err) return next(err);
-            const userId = user._id;
-            const token = jwt.sign({ userId }, "firstclass");
 
-            res.redirect("http://52.79.197.128/?token=" + token);
+            req.user = user;
+            next();
         }
     )(req, res, next);
 };
 
-router.get("/kakao/callback", kakaoCallback);
+router.get("/kakao/callback", kakaoCallback, generateToken, (req, res) => {
+    const token = res.locals.token;
+
+    res.redirect("http://localhost:3001/?token=" + token);
+});
 
 module.exports = router;
