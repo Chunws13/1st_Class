15cf180c6b2a604bf01bd6decb666cbebc828119
Chunws13 <dntkd51@naver.com>
2023-06-07 21:42:38 +0900

    1234

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -20,7 +20,7 @@ const kakaoCallback = (req, res, next) => {
 router.get("/kakao/callback", kakaoCallback, generateToken, (req, res) => {
     const token = res.locals.token;
 
-    res.redirect("http://localhost:3000/?token=" + token);
+    req.res.redirect("http://localhost:3000/?token=" + token);
 });
 
 module.exports = router;
