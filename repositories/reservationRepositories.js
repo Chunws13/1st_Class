266a9aa6247a751761981831d98b7c8c7bf69a63
Easton Park <128500186+cali-rocket@users.noreply.github.com>
2023-06-07 20:12:10 +0900

    Create reservationRepositories.js

---
+++ repositories/reservationRepositories.js
@@ -0,0 +1,48 @@
+const { Reservations } = require("../models");
+
+class ReservationsRepository {
+    findReservations = async (user_id, flight_id, people_num) => {
+        try {
+            const reservations = await Reservations.findAll({
+                attributes: [
+                    "user_id",
+                    "flight_id",
+                    "people_num",
+                    "total_price",
+                ],
+            });
+            if (reservations.length === 0) {
+                return res.status(400).json({ message: "예약 정보가 없습니다." });
+              }
+            return reservations;
+        } catch (error) {
+            const errorMessage = "예약 정보 조회에 실패했습니다.";
+            return errorMessage;
+        }
+    };
+
+    createReservations = async (user_id, flight_id, people_num) => {
+        try {
+            // 예약할 항공권 할당
+            const targetFlight = await Flights.findOne({
+                where: { flight_id },
+            });
+
+            const total_price = targetFlight.price * people_num;
+
+            const reservations = await Reservations.create({
+                user_id,
+                flight_id,
+                people_num,
+                total_price,
+            });
+            return reservations;
+        } catch (error) {
+            console.log(error);
+            const errorMessage = "reservation 오류";
+            return errorMessage;
+        }
+    };
+}
+
+module.exports = ReservationsRepository;
